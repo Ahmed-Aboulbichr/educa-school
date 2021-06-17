b78268e689e63cb84dc86e15618cf2823e874841
Anouar es-sayid <anouaressayid1999@gmail.com>
2021-06-17 21:18:42 +0100

    models and migrations added

---
+++ database/migrations/2021_06_16_174445_create_sous_groupes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSousGroupesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sous_groupes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sous_groupes');
+    }
+}
