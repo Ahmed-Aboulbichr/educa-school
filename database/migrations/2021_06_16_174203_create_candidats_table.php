0ea7379459809c7d3a0dc2d8cd4042d1451f7ad6
Ahmed <aboulbichr.ahmedAB@gmail.com>
2021-06-18 12:51:49 +0100

    ajout des controleurs suivantes [Administrateur, Candidature, Candidat, Etudiant, Formation, Professeur, Salle, Seance, Session]

--- database/migrations/2021_06_16_174203_create_candidats_table.php
+++ database/migrations/2021_06_16_174203_create_candidats_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCondidatsTable extends Migration
+class CreateCandidatsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCondidatsTable extends Migration
      */
     public function up()
     {
-        Schema::create('condidats', function (Blueprint $table) {
+        Schema::create('candidats', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateCondidatsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('condidats');
+        Schema::dropIfExists('candidats');
     }
 }
