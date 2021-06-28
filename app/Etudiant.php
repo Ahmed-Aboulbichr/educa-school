1fbc299413a0374df02bb133d25d977601b1dc6f
Anouar es-sayid <anouaressayid1999@gmail.com>
2021-06-28 03:40:47 +0100

    final changes

--- app/Etudiant.php
+++ app/Etudiant.php
@@ -13,18 +13,3 @@ class Etudiant extends User
 }
 
 
-/*$envelopeApi = new DocuSign\eSign\Api\EnvelopesApi($apiClient);
-// assign recipient to template role by setting name, email, and role name. Note that the
-// template role name must match the placeholder role name saved in your account template.
-$templateRole = new DocuSign\eSign\Model\TemplateRole();
-$templateRole->setEmail("betty@example.com");
-$templateRole->setName("Betty Wallace");
-$templateRole->setRoleName("signer");
-
-// instantiate a new envelope object and configure settings
-$envelop_definition = new DocuSign\eSign\Model\EnvelopeDefinition();
-$envelop_definition->setEmailSubject("[DocuSign PHP SDK] - Signature Request Sample");
-$envelop_definition->setTemplateId("703bb5d4-xxxx-xxxx-xxxx-83b43b28c384");
-$envelop_definition->setTemplateRoles(array($templateRole));
-
-// set envelope status to "sent" to immediately send the signature request*/
