bf4e9f8348d63171e089bdf57921db362af481a5
Ahmed <aboulbichr.ahmedAB@gmail.com>
2021-07-14 23:31:45 +0100

    adding cursus link

--- resources/views/layouts/sidebar-educa-etud.blade.php
+++ resources/views/layouts/sidebar-educa-etud.blade.php
@@ -16,6 +16,7 @@
                     <ul class="sub-menu" aria-expanded="false">
                         <li><a href={{route('getPreInscr')}}>Pre-inscription</a></li>
                         <li><a href={{route('profile')}}>Profile</a></li>
+                        <li><a href={{route('cursus_universitaire')}}>mon parcours</a></li>
                         <li>
                             <a href="javascript: void(0);" class="has-arrow waves-effect">
                                
