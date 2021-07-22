22a24786b1e5c92f3420a7edca0d15bba885cc41
YASSINE IGUIDRE <54177393+YassineIGUIDRE@users.noreply.github.com>
2021-07-22 19:05:04 +0100

    mise en forme complete espace candidat

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,74 +11,88 @@ Register
 </div>
 <div>
     <div class="container-fluid p-0">
-        <div class="row no-gutters">
+        <div class="row">
             <div class="col-lg-4">
                 <div class="authentication-page-content p-4 d-flex align-items-center min-vh-100">
                     <div class="w-100">
                         <div class="row justify-content-center">
                             <div class="col-lg-9">
                                 <div>
-                                    <div class="text-center">
+                                    <div class="text-center ">
                                         <div>
-                                            <a href="{{url('index')}}" class="logo"><img src="{{ URL::asset('/assets/images/logo.png')}}" height="20" alt="logo"></a>
+                                            <a href="{{url('index')}}" class="logo"><img src="{{ URL::asset('/assets/images/logo.png')}}" height="70" alt="logo"></a>
                                         </div>
-                                        <h4 class="font-size-18 mt-4">Register account</h4>
+                                        <h4 class="font-size-18 mt-5">Register account</h4>
                                     </div>
 
                                     <div class="p-2 mt-5">
                                         <form method="POST" action="{{ route('register') }}">
                                             @csrf
-                                            <!--
-                                            <div class="input-daterange input-group" data-provide="datepicker" data-date-format="dd M, yyyy" data-date-autoclose="true">
-                                                <input type="text" class="form-control" name="start" />
-                                                <input type="text" class="form-control" name="end" />
-                                            </div> !-->
-                                            <div class="form-group auth-form-group-custom mb-4">
-                                                <i class="ri-user-2-line auti-custom-input-icon"></i>
-                                                <label for="firstName">{{ __('firstName') }}</label>
-                                                <input type="text" id="firstName" class="form-control @error('firstName') is-invalid @enderror" name="firstName" value="{{ old('firstName') }}" required autocomplete="firstName" autofocus placeholder="Enter name">
-                                                @error('firstName')
-                                                    <span class="invalid-feedback" role="alert">
-                                                        <strong>{{ $message }}</strong>
-                                                    </span>
-                                                @enderror
-                                            </div>
-                                            <div class="form-group auth-form-group-custom mb-4">
-                                                <i class="ri-user-2-line auti-custom-input-icon"></i>
-                                                <label for="lastName">{{ __('lastName') }}</label>
-                                                <input type="text" id="lastName" class="form-control @error('lastName') is-invalid @enderror" name="lastName" value="{{ old('lastName') }}" required autocomplete="lastName" autofocus placeholder="Enter name">
-                                                @error('lastName')
-                                                    <span class="invalid-feedback" role="alert">
-                                                        <strong>{{ $message }}</strong>
-                                                    </span>
-                                                @enderror
-                                            </div>
-                                            <div class="form-group auth-form-group-custom mb-4">
-                                                <i class="ri-mail-line auti-custom-input-icon"></i>
-                                                <label for="email">{{ __('E-Mail Address') }}</label>
-                                                <input type="email" id="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Enter email">
-                                                @error('email')
-                                                    <span class="invalid-feedback" role="alert">
-                                                        <strong>{{ $message }}</strong>
-                                                    </span>
-                                                @enderror
+                                            <div class="row mb-2">
+                                                <div class="col-lg-2">
+                                                    <i style="color:#5664d2;font-size:1.7em;" class="ri-user-2-line auti-custom-input-icon"></i>
+                                                </div>
+                                                <div class="col-lg-10">
+                                                    <div class="form-group mb-4">
+                                                        <div class="input-daterange input-group" data-provide="datepicker" data-date-format="dd M, yyyy" data-date-autoclose="true">
+                                                            <input type="text" id="firstName" class="form-control @error('firstName') is-invalid @enderror" name="firstName" value="{{ old('firstName') }}" required autocomplete="firstName" autofocus placeholder="First name">
+                                                                @error('firstName')
+                                                                    <span class="invalid-feedback" role="alert">
+                                                                        <strong>{{ $message }}</strong>
+                                                                    </span>
+                                                                @enderror
+                                                            <input type="text" id="lastName" class="form-control @error('lastName') is-invalid @enderror" name="lastName" value="{{ old('lastName') }}" required autocomplete="lastName" autofocus placeholder="Last name">
+                                                                @error('lastName')
+                                                                    <span class="invalid-feedback" role="alert">
+                                                                        <strong>{{ $message }}</strong>
+                                                                    </span>
+                                                                @enderror
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
 
-                                            <div class="form-group auth-form-group-custom mb-4">
-                                                <i class="ri-lock-2-line auti-custom-input-icon"></i>
-                                                <label for="password">{{ __('Password') }}</label>
-                                                <input type="password" id="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Enter password">
-                                                @error('password')
-                                                    <span class="invalid-feedback" role="alert">
-                                                        <strong>{{ $message }}</strong>
-                                                    </span>
-                                                @enderror
+                                            <div class="row mb-2">
+                                                <div class="col-lg-2">
+                                                    <i style="color:#5664d2;font-size:1.7em;" class="ri-mail-line auti-custom-input-icon"></i>
+                                                </div>
+                                                <div class="col-lg-10">
+                                                    <div class="form-group">
+                                                        <input type="email" id="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Enter email">
+                                                        @error('email')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                        @enderror
+                                                    </div>
+                                                </div>
                                             </div>
 
-                                            <div class="form-group auth-form-group-custom mb-4">
-                                                <i class="ri-lock-2-line auti-custom-input-icon"></i>
-                                                <label for="password-confirm">{{ __('Confirm Password') }}</label>
-                                                <input type="password" id="password-confirm" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Enter password">
+                                            <div class="row mb-2">
+                                                <div class="col-lg-2">
+                                                    <i style="color:#5664d2;font-size:1.7em;"  class="ri-lock-2-line auti-custom-input-icon"></i>
+                                                </div>
+                                                <div class="col-lg-10">
+                                                    <div class="form-group">
+                                                        <input type="password" id="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Enter password">
+                                                        @error('password')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                        @enderror
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="row mb-2">
+                                                <div class="col-lg-2">
+                                                    <i style="color:#5664d2;font-size:1.7em;"  class="ri-lock-2-line auti-custom-input-icon"></i>
+                                                </div>
+                                                <div class="col-lg-10">
+                                                    <div class="form-group">
+                                                        <input type="password" id="password-confirm" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Confirm password">
+                                                    </div>
+                                                </div>
                                             </div>
 
                                             <div class="text-center">
@@ -92,9 +106,8 @@ Register
                                         </form>
                                     </div>
 
-                                    <div class="mt-5 text-center">
-                                        <p>Don't have an account ? <a href="{{url('login')}}" class="font-weight-medium text-primary"> Login</a> </p>
-                                        <p><script>document.write(new Date().getFullYear())</script>© Nazox.  Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign</p>
+                                    <div class="mt-3 text-center">
+                                        <p><script>document.write(new Date().getFullYear())</script>© ENCGF</p>
                                     </div>
                                 </div>
                             </div>
