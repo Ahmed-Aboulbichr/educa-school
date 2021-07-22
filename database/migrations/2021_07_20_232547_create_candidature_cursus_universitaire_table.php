0fee70aa328ec1fdb5037363911ffaaedd1cb1ba
Anouar es-sayid <anouaressayid1999@gmail.com>
2021-07-22 15:38:50 +0100

    merged things

--- database/migrations/2021_07_20_232547_create_candidature_cursus_universitaire_table.php
+++ database/migrations/2021_07_20_232547_create_candidature_cursus_universitaire_table.php
@@ -4,11 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-<<<<<<< HEAD:database/migrations/2021_07_19_223616_create_candidature_cursus_universitaires.php
-class CreateCandidatureCursusUniversitairesTable extends Migration
-=======
 class CreateCandidatureCursusUniversitaireTable extends Migration
->>>>>>> 11ee12ae3e982033533b624bf1dbda40028988d8:database/migrations/2021_07_20_232547_create_candidature_cursus_universitaire_table.php
 {
     /**
      * Run the migrations.
@@ -17,11 +13,7 @@ class CreateCandidatureCursusUniversitaireTable extends Migration
      */
     public function up()
     {
-<<<<<<< HEAD:database/migrations/2021_07_19_223616_create_candidature_cursus_universitaires.php
-        Schema::create('candidature_cursus_universitaires', function (Blueprint $table) {
-=======
         Schema::create('candidature_cursus_universitaire', function (Blueprint $table) {
->>>>>>> 11ee12ae3e982033533b624bf1dbda40028988d8:database/migrations/2021_07_20_232547_create_candidature_cursus_universitaire_table.php
             $table->id();
             //columns foreign key
             $table->foreignId('candidature_id')->constrained()->onDelete('cascade');
